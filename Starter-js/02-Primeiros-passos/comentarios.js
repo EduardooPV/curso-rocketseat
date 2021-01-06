42bdcaeb3919d50f25f7b079e608a735c52032bb
Luiz Eduardo Prado Veltroni <eduardoveltroni@hotmail.com>
2021-01-05 22:11:47 -0300

    finished topic 2

---
+++ Starter-js/02-Primeiros-passos/comentarios.js
@@ -0,0 +1,7 @@
+// Aqui eu tenho um comentário em linha
+
+/*
+Aqui eu tenho um comentário multi linha / comentário de bloco
+O Código abaixo escreve uma mensagem no console.
+*/
+console.log("Bem vindos ao Starter") // Da pra comentar assim tambem.
