d0aa9d611d739f575da308209195fddc9074d818
Luiz Eduardo <eduardoveltroni@hotmail.com>
2021-01-12 21:16:05 -0300

    starter topic 5 js

---
+++ Starter-js/06-Funcoes/functions.js
@@ -0,0 +1,14 @@
+// Criar um aplicativo de frases motivacionais
+
+
+// Declaration - Declaração da função
+// Function statement
+function createPhrases() {
+    console.log('Estudar é muito bom')
+    console.log('Paciência e persistência')
+    console.log('Revisão é mãe do aprendizado')
+}
+
+// Executar a função
+// Rodar, chamar, invocar
+// execute, run, call, invoke
