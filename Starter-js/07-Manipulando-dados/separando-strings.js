324fb93b59619127977094e7fda2381c3319f895
Eduardo <eduardoveltroni@hotmail.com>
2021-05-06 14:45:28 -0300

    Organized

--- Starter-js/07-Manipulando-dados/separando-strings.js
+++ Starter-js/07-Manipulando-dados/separando-strings.js
@@ -1,10 +1,34 @@
-// Separando um texto que contem espaços, em um novo array ondce cada texto é uma posição do array. e dps colocar _ em cada espaço
+// Primeira maneira usando SWITCH
+function calculadoraAdicaoSubtracao (numero, outroNumero, operacao) {
+    let resultado = 0 
+    switch (operacao) {
+        case '+':
+            resultado = numero + outroNumero
+            break;
+        
+        case '-':
+            resultado = numero - outroNumero
+            break;
 
-let phrese = "Eu quero viver o Amor"
-let myArray = phrese.split(" ")
+        default:
+            resultado = 0
+    }
 
-console.log(myArray)
+    return resultado
+}
 
-let phreseWithUnderscore = myArray.join("_")
+console.log(calculadoraAdicaoSubtracao(5,5,"+"))
 
-console.log(phreseWithUnderscore.toLowerCase())
+// Segunda maneira usando Else If
+function calculadoraAdicaoSubtracao2 (numero, outroNumero, operacao) {
+    resultado = 0 
+    if (operacao == "+") {
+        resultado = numero + outroNumero
+    } else if (operacao == "-") {
+        resultado = numero - outroNumero
+    }
+
+    return resultado
+}
+
+console.log(calculadoraAdicaoSubtracao2(10,5,"-"))
